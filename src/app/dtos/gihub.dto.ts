fbfdbf2a850ddaf9748400b67ecf7ec3bf234dee
Gabriel <gabriel.slmendoza@gmail.com>
2024-06-26 20:27:02 -0500

    changing dto location

---
+++ src/app/dtos/gihub.dto.ts
@@ -0,0 +1,48 @@
+import { UserModel } from "../models"
+
+export interface GithubResponseDTO {
+	documentation_url: string
+	message: string
+	status: string
+}
+
+export interface GitHubUserListDTO {
+	total_count: number,
+	incomplete_results: boolean,
+	items: UserModel[]
+}
+
+export interface GitHubUserProfileDTO {
+	login: string,
+	id: number,
+	node_id: string,
+	avatar_url: string,
+	gravatar_id: string,
+	url: string,
+	html_url: string,
+	followers_url: string,
+	following_url: string,
+	gists_url: string,
+	starred_url: string,
+	subscriptions_url: string,
+	organizations_url: string,
+	repos_url: string,
+	events_url: string,
+	received_events_url: string,
+	type: string,
+	site_admin: boolean,
+	name: string,
+	company?: string,
+	blog: string,
+	location?: string,
+	email?: string,
+	hireable?: boolean,
+	bio: string,
+	twitter_username?: string,
+	public_repos: number,
+	public_gists: number,
+	followers: number,
+	following: number,
+	created_at: string,
+	updated_at: string
+}
--- src/app/services/user-list/user-list.dto.ts
+++
@@ -1,7 +0,0 @@
-import { UserModel } from "src/app/models/user.model";
-
-export interface GitHubUserListDTO {
-    total_count: number,
-    incomplete_results: boolean,
-    items: UserModel[]
-}
