cdb4e061fdb8c30589aa96bb296f42a75bc8d236
Gabriel <gabriel.slmendoza@gmail.com>
2026-09-16 03:16:23 -0500

    fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: false,
+	baseHref: '/',
 	githubAPIUrl: 'https://api.github.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: true,
+	baseHref: '/docs/',
 	githubAPIUrl: 'https://api.github.com'
 };
